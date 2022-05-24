92860069634480251f014efa0d893b0310cfcd0e
dohyun_kim <dohyun_kim4@tmax.co.kr>
2022-05-24 18:15:16 +0900

    [feat] router 설정 및 회원가입 페이지 추가, 스플리팅을 위해 loadable 설치 & style을 위해 emotion 설치

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const LogIn = loadable(() => import('../pages/Login'));
+const SignUp = loadable(() => import('../pages/SignUp'));
+
 const App = () => {
-    return <div>좀만 dd 기둘~ㅋ</div>;
+    return (
+        <Switch>
+            <Redirect exact path="/" to="/login" />
+            <Route path="/login" component={LogIn} />
+            <Route path="/signup" component={SignUp} />
+        </Switch>
+    );
 };
 
 export default App;
