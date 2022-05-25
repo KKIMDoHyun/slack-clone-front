02184eb5b35785531477e61c94ebfae7c956783a
dohyun_kim <dohyun_kim4@tmax.co.kr>
2022-05-25 17:46:00 +0900

    [feat] 사용자 정보 모달 구현, workspace 틀 구현

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -1,11 +1,27 @@
-import { Header } from '@layouts/Workspace/styles';
+import {
+    Channels,
+    Chats,
+    Header,
+    MenuScroll,
+    ProfileImg,
+    ProfileModal,
+    RightMenu,
+    WorkspaceName,
+    Workspaces,
+    WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { FC, useCallback } from 'react';
-import { Redirect } from 'react-router';
+import React, { FC, useCallback, useState } from 'react';
+import { Redirect, Route } from 'react-router';
 import useSWR from 'swr';
+import gravatar from 'gravatar';
+import Menu from '@components/Menu';
+import DirectMessage from '@pages/DirectMessage';
+import Channel from '@pages/Channel';
 
 const Workspace: FC = ({ children }) => {
+    const [showUserMenu, setShowUserMenu] = useState(false);
     const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher);
 
     const onLogout = useCallback(() => {
@@ -18,15 +34,47 @@ const Workspace: FC = ({ children }) => {
                 mutate(res.data, false);
             });
     }, []);
-
+    const onClickUserProfile = useCallback(() => {
+        setShowUserMenu((prev) => !prev);
+    }, []);
     if (!data) {
         return <Redirect to="/login" />;
     }
     return (
         <div>
-            <Header>test</Header>
+            <Header>
+                <RightMenu>
+                    <span onClick={onClickUserProfile}>
+                        <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.nickname} />
+                        {showUserMenu && (
+                            <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                                <ProfileModal>
+                                    <img
+                                        src={gravatar.url(data.nickname, { s: '36px', d: 'retro' })}
+                                        alt={data.nickname}
+                                    />
+                                    <div>
+                                        <span id="profile-name">{data.nickname}</span>
+                                        <span id="profile-active">Active</span>
+                                    </div>
+                                </ProfileModal>
+                            </Menu>
+                        )}
+                    </span>
+                </RightMenu>
+            </Header>
             <button onClick={onLogout}>로그아웃</button>
-            {children}
+            <WorkspaceWrapper>
+                <Workspaces>test</Workspaces>
+                <Channels>
+                    <WorkspaceName>SLACK</WorkspaceName>
+                    <MenuScroll>menu scroll</MenuScroll>
+                </Channels>
+                <Chats>
+                    {/* <Route path="/workspace/channel" component={Channel} />
+                    <Route path="/workspace/dm" component={DirectMessage} /> */}
+                </Chats>
+            </WorkspaceWrapper>
         </div>
     );
 };
---
+++ pages/DirectMessage/index.tsx
@@ -0,0 +1,12 @@
+import Workspace from '@layouts/Workspace';
+import React from 'react';
+
+const DirectMessage = () => {
+    return (
+        <Workspace>
+            <div>로그인ㅊㅊ</div>
+        </Workspace>
+    );
+};
+
+export default DirectMessage;
